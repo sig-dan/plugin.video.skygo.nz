52deba1afab01e0846a14c56355922b6d89ac916
Matt Huisman <me@matthuisman.nz>
2018-10-17 16:03:15 +1300

    Migrate to latest common

--- resources/lib/language.py
+++ resources/lib/language.py
@@ -1,20 +1,15 @@
 from matthuisman.language import BaseLanguage
 
 class Language(BaseLanguage):
-    LOGIN            = 30000
-    HIDE_CHANNEL     = 30001
-    LOGOUT           = 30002
-    SETTINGS         = 30003
-    ASK_USERNAME     = 30004
-    ASK_PASSWORD     = 30005
-    LOGIN_ERROR      = 30006
-    LOGOUT_YES_NO    = 30007
-    NO_CHANNEL       = 30008
-    NO_STREAM        = 30009
-    ADOBE_ERROR      = 30010
-    TOKEN_ERROR      = 30011
-    LIVE_TV          = 30012
-    RESET_HIDDEN     = 30013
-    RESET_HIDDEN_OK  = 30014
+    HIDE_CHANNEL     = 30000
+    ASK_USERNAME     = 30001
+    ASK_PASSWORD     = 30002
+    NO_CHANNEL       = 30003
+    NO_STREAM        = 30004
+    ADOBE_ERROR      = 30005
+    TOKEN_ERROR      = 30006
+    LIVE_TV          = 30007
+    RESET_HIDDEN     = 30008
+    RESET_HIDDEN_OK  = 30009
 
 _ = Language()
